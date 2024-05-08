6301103ea4c282965b114705d15a7e7b56837983
JustasPolis <j.polikevicius@gmail.com>
2024-05-08 20:17:40 +0300

    init

---
+++ App/Main.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import AppFeature
+
+@main
+struct Main: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Modules/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let package = Package(
+  name: "Modules",
+  platforms: [.iOS(.v15)],
+  products: [
+    .singleTargetLibrary("AppFeature")
+  ],
+  dependencies: [
+    .package(url: "https://github.com/krzysztofzablocki/Inject.git", exact: "1.5.0"),
+    .package(url: "https://github.com/krzysztofzablocki/Difference.git", exact: "1.0.2"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", exact: "1.12.0"),
+  ],
+  targets: [
+    .target(name: "AppFeature", dependencies: []),
+  ]
+)
+
+extension Product {
+  static func singleTargetLibrary(_ name: String) -> Product {
+    .library(name: name, targets: [name])
+  }
+}
