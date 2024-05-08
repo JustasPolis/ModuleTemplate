a16406b7eb69e23cfbcc614f2a9c7324221ed871
JustasPolis <j.polikevicius@gmail.com>
2024-05-08 22:00:38 +0300

    tests

--- Modules/Tests/ModelsTests/TestModelTests.swift
+++ Modules/Tests/ModelsTests/TestModelTests.swift
@@ -7,4 +7,8 @@ final class TestModelTests: XCTestCase {
   func test_smoke() {
     XCTAssertEqual([1, 2, 3], [1, 2, 3])
   }
+
+  func test_something_else() {
+    XCTAssertEqual(true, true)
+  }
 }
